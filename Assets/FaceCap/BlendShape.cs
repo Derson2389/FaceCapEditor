d2d748ea11012d76bd3c41aa14842606d520e73b
Derson2389 <422696858@qq.com>
2018-11-05 23:00:37 +0800

    对对对

---
+++ Assets/FaceCap/BlendShape.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class BlendShape : System.Object
+{
+    /// <summary>
+    /// The blendable indeces.
+    /// </summary>
+    [SerializeField]
+    public int blendableIndex;
+
+    /// <summary>
+    /// The blendable name. Used for re-syncing
+    /// </summary>
+    [SerializeField]
+    public string blendableName;
+
+    /// <summary>
+    /// The associated weight.
+    /// </summary>
+    [SerializeField]
+    public float weight;
+}
+
--- Assets/FaceCap/Script/FaceControllerComponent.cs
+++ Assets/FaceCap/Script/FaceControllerComponent.cs
@@ -6,13 +6,31 @@ using UnityEngine;
 [ExecuteInEditMode]
 public class FaceControllerComponent : MonoBehaviour {
 
+    private SkinnedMeshRenderer SkinMesh = null;
+    public List<BlendShape> blendShapeList = new List<BlendShape>();
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        SkinMesh = this.GetComponent<SkinnedMeshRenderer>();
+        if (SkinMesh != null)
+        {
+            int blendShapesCnt = SkinMesh.sharedMesh.blendShapeCount;
+            for (int idx = 0; idx < blendShapesCnt; idx++)
+            {
+                BlendShape newBlend = new BlendShape();
+                newBlend.blendableIndex = idx;
+                newBlend.blendableName = SkinMesh.sharedMesh.GetBlendShapeName(idx);
+                newBlend.weight = SkinMesh.GetBlendShapeWeight(idx);
+                blendShapeList.Add(newBlend);
+            }
+        }
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        
+
+
 	}
 }
